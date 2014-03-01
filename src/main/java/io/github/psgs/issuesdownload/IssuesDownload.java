7ed2f69b4c59116a651260be2f0ad6a512b3dec0
psgs <fpigerre@gmail.com>
2014-03-01 20:27:58 +1000

    Remove use of OpenCSV library

--- src/main/java/io/github/psgs/issuesdownload/IssuesDownload.java
+++ src/main/java/io/github/psgs/issuesdownload/IssuesDownload.java
@@ -1,21 +1,21 @@
 package io.github.psgs.issuesdownload;
 
-import au.com.bytecode.opencsv.CSVWriter;
 import io.github.psgs.issuesdownload.gui.GUI;
 import org.eclipse.egit.github.core.Issue;
 import org.eclipse.egit.github.core.client.GitHubClient;
 import org.eclipse.egit.github.core.service.IssueService;
 
-import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Collections;
+import java.util.List;
 
 public class IssuesDownload {
 
     public static void main(String[] args) {
         try {
-        Config.loadConfiguration();
-        } catch(IOException ex) {
+            Config.loadConfiguration();
+        } catch (IOException ex) {
 
         }
         GUI.main(args);
@@ -29,26 +29,38 @@ public class IssuesDownload {
         GitHubClient client = new GitHubClient();
         client.setCredentials(Config.githubuser, Config.githubpass);
 
-        IssueService issueService = new IssueService(client);
-
         try {
-            CSVWriter writer = new CSVWriter(new FileWriter("issues.csv"), '\t');
-            String[] header = {"Id", "Title", "Creator", "Assignee", "Milestone", "State", "Body Text"};
-            writer.writeNext(header);
+            FileWriter writer = new FileWriter("issues.csv");
+            writer.append("Id, Title, Creator, Assignee, Milestone, State, Body Text");
+            writer.append("\n");
 
-            for (Issue issue : issueService.getIssues(repoOwner, repoName, null)) {
-                String[] data = {String.valueOf(issue.getId()), issue.getTitle(), issue.getUser().getName(), issue.getAssignee().getName(), issue.getMilestone().getTitle(), issue.getState(), issue.getBodyText()};
-                writer.writeNext(data);
+            for (Issue issue : getAllIssues(client, repoOwner, repoName)) {
+                writer.append(String.valueOf(issue.getId()) + ",");
+                writer.append(issue.getTitle() + ",");
+                writer.append(issue.getUser().getName() + ",");
+                writer.append(issue.getAssignee().getName() + ",");
+                writer.append(issue.getMilestone().getTitle() + ",");
+                writer.append(issue.getState() + ",");
+                writer.append(issue.getBodyText());
+                writer.append("\n");
             }
+            writer.flush();
             writer.close();
             return "Download Complete!";
         } catch (IOException ex) {
             System.out.println("An IOException has occured!");
             ex.printStackTrace();
             if (ex.getMessage().equalsIgnoreCase("api.github.com")) {
-                return "An error has occured, reaching " + ex.getMessage() + "! Please check your network connection.";
+                return "An error has occurred reaching " + ex.getMessage() + "! Please check your network connection.";
             }
         }
         return "An error has occured!";
+    }
+
+    public static List<Issue> getAllIssues(GitHubClient client, String repoOwner, String repoName) throws IOException {
+        IssueService service = new IssueService(client);
+        return service.getIssues(repoOwner, repoName,
+                Collections.singletonMap(IssueService.FILTER_STATE,
+                        IssueService.STATE_OPEN));
     }
 }
